97ae6bd7d21eb27ec3d178beff3e49abd4347bb9
Xas <xas@dipietro.net>
2019-10-13 00:47:33 +0200

    Small BEncode test

---
+++ Source/RatioMaster.Core.Tests/BEncodeTests.cs
@@ -0,0 +1,26 @@
+﻿using BitTorrent;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RatioMaster.Core.Tests
+{
+    [TestFixture]
+    class BEncodeTests
+    {
+        [OneTimeSetUp]
+        public void Init()
+        {
+            EncodingProvider codePages = CodePagesEncodingProvider.Instance;
+            Encoding.RegisterProvider(codePages);
+        }
+
+        [Test]
+        public void ValueStringTest()
+        {
+            ValueString vs = new ValueString();
+            Assert.That(vs.Bytes, Is.EqualTo(new byte[0]));
+        }
+    }
+}
